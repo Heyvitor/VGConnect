09ecbceb31c2a5f5c02f950ccf21460bad1359d0
kingvitor01@gmail.com <kingvitor01@gmail.com>
2025-03-19 04:11:00 -0300

    update 1.2

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,4 @@
-const Agent = require('./Agent');
+const Agent = require('./agent');
 const User = require('./user');
 const providerKey = require('./providerKey');
 const providerGame = require('./providerGame');
